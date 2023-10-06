963594660acdab62ed702f8ea80d39c627767d44
David P <david.purge@gmail.com>
2023-10-06 14:36:25 +0200

    Create main function for Linux script

--- src/cli/jdp_cli/__main__.py
+++ src/cli/jdp_cli/__main__.py
@@ -2,6 +2,9 @@ import sys
 from knack import CLI
 from .commands_loader import JdpCommandsLoader
 
-jdp_cli = CLI(cli_name='jdp', commands_loader_cls = JdpCommandsLoader)
-exit_code = jdp_cli.invoke(sys.argv[1:])
-sys.exit(exit_code)
+def main():
+    jdp_cli = CLI(cli_name='jdp', commands_loader_cls = JdpCommandsLoader)
+    exit_code = jdp_cli.invoke(sys.argv[1:])
+    sys.exit(exit_code)
+
+main()
